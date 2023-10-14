eb1cdb972016ae2574f3cbd9c97139ff4d4bc79c
Chamathka Sewwandi <112297760+chamathkas1227@users.noreply.github.com>
2023-10-14 11:18:38 +0530

    Update pro1py1.py

--- pro1py1.py
+++ pro1py1.py
@@ -1,2 +1,11 @@
 print("Hello, World!")
 print("This is the first file of project 1 branch")
+
+#modification1
+x = input()
+x = int(x)
+a = x%2
+if a == 1:
+  print("The number you entered is a Odd number")
+else:
+  print("The number you entered is a Even number")
