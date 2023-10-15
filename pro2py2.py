9a54f89bd8d538561980812eb8acf87c3c3265ca
Chamathka Sewwandi <112297760+chamathkas1227@users.noreply.github.com>
2023-10-15 14:29:11 +0530

    Added codes for Assigning Grades

--- pro2py2.py
+++ pro2py2.py
@@ -1,2 +1,19 @@
 print("Hello, World!")
 print("This is the second file of project 2 branch")
+
+#modification
+# Get the student's score from user input
+score = float(input("Enter the student's score: "))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"The student's grade is: {grade}")
