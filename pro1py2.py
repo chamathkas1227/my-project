a5d70985b0041db9965bdd34c4f5526ddd5531e1
Sithum Hettiarachchi <131165544+Sithum0108@users.noreply.github.com>
2023-10-15 10:01:22 +0530

    Update pro1py2.py

--- pro1py2.py
+++ pro1py2.py
@@ -4,3 +4,10 @@ print("This is the second file of project 1 branch")
 #modification1
 for x in range(5):
   print(x)
+
+#modification2: Checking if numbers are even or odd
+for x in range(5):
+    if x % 2 == 0:
+        print(f"{x} is even")
+    else:
+        print(f"{x} is odd")
